62b616a1840aaa5bb331081b40d058c27656ca38
Eric Bredder <121258349+eb8ga@users.noreply.github.com>
2023-11-30 01:21:39 -0500

    Update roversa.ts

--- roversa.ts
+++ roversa.ts
@@ -36,13 +36,13 @@ enum RoversaEvent {
  * Blocks for driving the Roversa robot
  */
 //% weight=100 color=#d55e00 icon="\uf085" block="Roversa"
-namespace kitronik_servo_lite {
+namespace roversa {
     /**
      * **********************************************************************************************************************************************
-     * micro:bit Servo:Lite / :MOVE mini blocks
+     * micro:bit roversa
      ************************************************************************************************************************************************/
 
-    /* Some parameters used for controlling the turn and length of the ServoLite board controlled :MOVE mini */
+    /* Some parameters used for controlling the turn and length of the roversa robot */
     const milliSecInASecond = 1000
     let distancePerSec = 100
     let numberOfDegreesPerSec = 200
@@ -51,7 +51,7 @@ namespace kitronik_servo_lite {
 	/**
 	 * 
 	 */
-    //% shim=kitronik_servolite::init
+    //% shim=roversa::init
     function init(): void {
         return;
     }
@@ -61,7 +61,7 @@ namespace kitronik_servo_lite {
 	 * @param button the pin that acts as a button
 	 */
     //% weight=89
-    //% blockId=kitronik_servolite_ispressed block="Roversa button %button|is pressed"
+    //% blockId=roversa_ispressed block="Roversa button %button|is pressed"
     //% group="Buttons" weight=91
     //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
     export function isPressed(button: RoversaPin): boolean {
@@ -74,7 +74,7 @@ namespace kitronik_servo_lite {
 	 * Registers code to run when a Roversa button is detected.
 	 */
     //% weight=90
-    //% blockId=kitronik_servolite_onevent block="Roversa button on %button|%event"
+    //% blockId=roversa_onevent block="Roversa button on %button|%event"
     //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
     //% event.fieldEditor="gridpicker" event.fieldOptions.columns=3
     export function onEvent(button: RoversaPin, event: RoversaEvent, handler: Action) {
@@ -86,7 +86,7 @@ namespace kitronik_servo_lite {
      * Apply a bias to the wheels. 0 to 50 for left, 50 to 100 for right.
      * @param bias eg: 50
      */
-    //% blockId=kitronik_servolite_servos_bias
+    //% blockId=roversa_servos_bias
     //% group="Buttons" weight=88
     //% block="bias %biasDriving"
     //% bias.min=0 bias.max=100
@@ -102,7 +102,7 @@ namespace kitronik_servo_lite {
     /**
      * Drives forwards. Call stop to stop
      */
-    //% blockId=kitronik_servolite_servos_forward
+    //% blockId=roversa_servos_forward
     //% block="drive forward"
     export function forward(): void {
         let P1Output = 0;
@@ -123,7 +123,7 @@ namespace kitronik_servo_lite {
     /**
      * Drives backwards. Call stop to stop
      */
-    //% blockId=kitronik_servolite_servos_backward
+    //% blockId=roversa_servos_backward
     //% block="drive backward"
     export function backward(): void {
         let P1Output = 180;
@@ -144,7 +144,7 @@ namespace kitronik_servo_lite {
     /**
     * Turns left. Call stop to stop
     */
-    //% blockId=kitronik_servolite_servos_left
+    //% blockId=roversa_servos_left
     //% block="turn left"
     export function left(): void {
         pins.servoWritePin(AnalogPin.P1, 0);
@@ -154,7 +154,7 @@ namespace kitronik_servo_lite {
     /**
      * Turns right. Call ``stop`` to stop
      */
-    //% blockId=kitronik_servolite_servos_right
+    //% blockId=roversa_servos_right
     //% block="turn right"
     export function right(): void {
         pins.servoWritePin(AnalogPin.P1, 180);
@@ -168,7 +168,7 @@ namespace kitronik_servo_lite {
      * On a normal servo this will stop the servo where it is, rather than return it to neutral position.
      * It will also not provide any holding force.
      */
-    //% blockId=kitronik_servolite_servos_stop
+    //% blockId=roversa_servos_stop
     //% block="stop"
     export function stop(): void {
         pins.analogWritePin(AnalogPin.P1, 0);
@@ -179,7 +179,7 @@ namespace kitronik_servo_lite {
      * Sends servos to 'neutral' position.
      * On a well trimmed 360 this is stationary, on a normal servo this is 90 degrees.
      */
-    //% blockId=kitronik_servolite_servos_neutral
+    //% blockId=roversa_servos_neutral
     //% block="goto neutral position"
     export function neutral(): void {
         pins.servoWritePin(AnalogPin.P1, 90);
@@ -190,7 +190,7 @@ namespace kitronik_servo_lite {
      * Drives forwards the requested distance and then stops
      * @param howFar distance to move
      */
-    //% blockId=kitronik_servolite_drive_forwards
+    //% blockId=roversa_drive_forwards
     //% block="drive forwards %howFar|distance" 
     export function driveForwards(howFar: number): void {
         let timeToWait = (howFar * milliSecInASecond) / distancePerSec; // calculation done this way round to avoid zero rounding
@@ -203,7 +203,7 @@ namespace kitronik_servo_lite {
      * Drives backwards the requested distance and then stops
      * @param howFar distance to move
      */
-    //% blockId=kitronik_servolite_drive_backwards
+    //% blockId=roversa_drive_backwards
     //% block="drive backwards %howFar|distance" 
     export function driveBackwards(howFar: number): void {
         let timeToWait = (howFar * milliSecInASecond) / distancePerSec; // calculation done this way round to avoid zero rounding
@@ -219,7 +219,7 @@ namespace kitronik_servo_lite {
      * Runs the servos at slower than the right function to reduce wheel slip
      * @param deg how far to turn, eg: 90
      */
-    //% blockId=kitronik_servolite_turn_right
+    //% blockId=roversa_turn_right
     //% block="turn right %deg|degrees"
     export function turnRight(deg: number): void {
         let timeToWait = (deg * milliSecInASecond) / numberOfDegreesPerSec;// calculation done this way round to avoid zero rounding
@@ -236,7 +236,7 @@ namespace kitronik_servo_lite {
     * Runs the servos at slower than the right function to reduce wheel slip
     * @param deg how far to turn, eg: 90
     */
-    //% blockId=kitronik_servolite_turn_left
+    //% blockId=roversa_turn_left
     //% block="turn left %deg|degrees"
     export function turnLeft(deg: number): void {
         let timeToWait = (deg * milliSecInASecond) / numberOfDegreesPerSec;// calculation done this way round to avoid zero rounding
@@ -247,22 +247,22 @@ namespace kitronik_servo_lite {
     }
 
     /**
-     * Allows the setting of the :MOVE mini turn amount.
+     * Allows the setting of roversa turn amount.
      * This allows tuning for the turn x degrees commands
      * @param degPerSec : How many degrees per second the mini does.
      */
-    //% blockId=kitronik_servolite_set_turn_speed_param
+    //% blockId=roversa_set_turn_speed_param
     //% block="calibrate turn amount to %degPerSec|degrees per second" 
     export function setDegreesPerSecond(degPerSec: number): void {
         numberOfDegreesPerSec = degPerSec
     }
 
     /**
-     * Allows the setting of the :MOVE mini forward / reverse distance.
+     * Allows the setting of roversa forward / reverse distance.
      * This allows tuning for the move x distance commands
      * @param distPerSec : How many mm per second the mini does.
      */
-    //% blockId=kitronik_servolite_set_movement_speed_param 
+    //% blockId=roversa_set_movement_speed_param 
     //% block="calibrate drive amount to %distPerSec|mm per second"
     export function setDistancePerSecond(distPerSec: number): void {
         distancePerSec = distPerSec
