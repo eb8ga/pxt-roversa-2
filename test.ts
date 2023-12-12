9596c001e25746a49078705f0410c63e61711f84
Eric Bredder <121258349+eb8ga@users.noreply.github.com>
2023-12-12 18:12:03 -0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -11,3 +11,19 @@ input.onButtonPressed(Button.AB, () => {
     roversa.stop
 })
 
+basic.forever(() => {
+    if (gamerbit.isPressed(RoversaPin.P5))
+        led.scroll("P5 - Enter");
+    if (gamerbit.isPressed(RoversaPin.P8))
+        led.scroll("P8 - Play");
+    if (gamerbit.isPressed(RoversaPin.P9))
+        led.scroll("P9 - Stop");
+    if (gamerbit.isPressed(RoversaPin.P13))
+        led.scroll("P13 - Forward");
+    if (gamerbit.isPressed(RoversaPin.P14))
+        led.scroll("P14 - Reverse");
+    if (gamerbit.isPressed(RoversaPin.P15))
+        led.scroll("P15 - Right");
+    if (gamerbit.isPressed(RoversaPin.P16))
+        led.scroll("P16 - Left");
+})
