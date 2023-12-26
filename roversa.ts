e24d3516a00302ed4d4ec2c4dfdbd9248442f805
Eric Bredder <121258349+eb8ga@users.noreply.github.com>
2023-12-26 09:32:52 -0500

    Update roversa.ts

--- roversa.ts
+++ roversa.ts
@@ -51,9 +51,19 @@ namespace roversa {
 	/**
 	 * 
 	 */
-    function init(): void {
-        return;
+      let initialized = false;
+      function init() {
+        if (initialized) return;
+        pins.pushButton(DigitalPin.P5)
+        pins.pushButton(DigitalPin.P8)
+        pins.pushButton(DigitalPin.P9)
+        pins.pushButton(DigitalPin.P13)
+        pins.pushButton(DigitalPin.P14)
+        pins.pushButton(DigitalPin.P15)
+        pins.pushButton(DigitalPin.P16)
+        initialized = true;
     }
+}
 
 	/**
 	 * Determines if a button is pressed
